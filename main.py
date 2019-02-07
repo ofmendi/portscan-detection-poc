ab621c69dc2f984e045e1e4541a3c1d48aaabaa0
Omer Faruk MENDI <ofmendi@btrisk.com>
2019-02-07 10:47:54 +0300

    main fuction created

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import time
+import smtplib
+import keyring
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def main():
+    pass
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as err:
+        print(err)
